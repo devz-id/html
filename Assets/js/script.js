02aeda14d4a734296b3184f222bb17da895aa252
devz <82320632+devz-id@users.noreply.github.com>
2023-05-13 16:09:32 +0900

    Add files via upload

---
+++ Assets/js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#multiple-text', {
+	strings: ['frontend developer', 'backend developer', 'mahasiswa'],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true
+})
