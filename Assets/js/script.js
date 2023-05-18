3ef22b2268c8e7d1731ace294ba41cce4d21fbf6
devz <82320632+devz-id@users.noreply.github.com>
2023-05-18 15:58:29 +0900

    Add files via upload
    Add a css media screen for mobile landscape on 576px size.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,14 +7,28 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-// Typed Script
-var typed = new Typed('#multiple-text', {
-	strings: ['frontend developer', 'backend developer', 'mahasiswa'],
-	typeSpeed: 100,
-	backSpeed: 100,
-	backDelay: 1000,
-	loop: true
-})
 
+// Animation And Update Text
 
+let andElement = document.getElementById("and");
+function updateText() {
+  let screenWidth = window.innerWidth;
+  if (screenWidth <= 576) {
+    andElement.innerHTML = "i am a ";
+  } else {
+    andElement.innerHTML = "and i'm a ";
+  }
+  
+  andElement.insertAdjacentHTML('beforeend', '<span id="multiple-text"></span>');
 
+  let typed = new Typed('#multiple-text', {
+    strings: ['frontend developer', 'backend developer', 'mahasiswa'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+}
+
+updateText();
+window.addEventListener("resize", updateText);
