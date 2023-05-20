7d77c9e92da99ae42ea3fd8697e9bc3912156883
devz <82320632+devz-id@users.noreply.github.com>
2023-05-20 16:47:42 +0900

    responsive 576px dan sedikit javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -32,3 +32,36 @@ function updateText() {
 
 updateText();
 window.addEventListener("resize", updateText);
+
+
+// menu 
+
+let menuIcon = document.getElementById('menuIcon');
+let menuActive = document.getElementById('menuActive');
+let xicon = document.getElementById('xIcon');
+let body = document.querySelector('body')
+
+  function hideMenu(){
+    menuIcon.style.display = 'none';
+    menuActive.style.right = '0';
+    xicon.style.display = 'block';
+  }
+
+  menuIcon.addEventListener('click', hideMenu);
+
+
+
+  function hideXicon() {
+    xicon.style.display = 'none';
+    menuActive.style.right = '-100%';
+    menuIcon.style.display = 'block';
+  }
+
+
+  body.addEventListener('click', function(event) {
+  let clickedElement = event.target;
+  
+  if (clickedElement.id === 'xIcon' || clickedElement.tagName === 'DIV' || clickedElement.tagName === 'SECTION' || clickedElement.tagName === 'NAV' ) {
+    hideXicon();
+  }
+});
